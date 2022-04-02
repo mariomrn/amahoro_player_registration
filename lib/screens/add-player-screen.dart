17a46578c6db48ae7a595cb50ce4a7e28e09c100
udulq <mario.fellger@student.kit.edu>
2022-04-02 22:22:54 +0200

    add-player-screen.dart: added Text fiels for name and birthday picker

--- lib/screens/add-player-screen.dart
+++ lib/screens/add-player-screen.dart
@@ -8,11 +8,57 @@ class AddPlayerScreen extends StatefulWidget {
 }
 
 class _AddPlayerScreenState extends State<AddPlayerScreen> {
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+
+  _selectDate(BuildContext context) async {
+    final DateTime? selected = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2010),
+      lastDate: DateTime(2025),
+    );
+    if (selected != null && selected != selectedDate) {
+      setState(() {
+        selectedDate = selected;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Text(
-        'Index 0: Add',
+    return SafeArea(
+      child: Column(
+        children: [
+          TextField(
+            controller: firstNameController,
+          ),
+          TextField(
+            controller: lastNameController,
+          ),
+          TextButton(
+            child: const Text('Choose Birthday'),
+            onPressed: () {
+              _selectDate(context);
+            },
+          ),
+          Text(selectedDate.toIso8601String()),
+          TextButton(
+            child: const Text('Press me'),
+            onPressed: () {
+              // Player p0 = Player(
+              //   firstName: firstNameController.text,
+              //   lastName: lastNameController.text,
+              //   birthday: DateTime(2019),
+              //   id: 1737168,
+              // );
+              //TODO: Hier muss der erzeugte Spieler in die Datenbank gespielt werden
+              firstNameController.clear();
+              lastNameController.clear();
+            },
+          ),
+        ],
       ),
     );
   }
