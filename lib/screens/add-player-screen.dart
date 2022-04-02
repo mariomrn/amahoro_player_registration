44c89ecc5d395902c325fe56630ab4743f5dfc53
udulq <mario.fellger@student.kit.edu>
2022-04-02 21:50:26 +0200

    deleted comments, added folder for screens

---
+++ lib/screens/add-player-screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddPlayerScreen extends StatefulWidget {
+  const AddPlayerScreen({Key? key}) : super(key: key);
+
+  @override
+  _AddPlayerScreenState createState() => _AddPlayerScreenState();
+}
+
+class _AddPlayerScreenState extends State<AddPlayerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(),
+    );
+  }
+}
