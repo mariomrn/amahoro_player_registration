a17d8339468d543f68223af890cbfd0e2f18670b
mario <mariomarin@hotmail.de>
2023-02-10 15:34:49 +0100

    removed image compression and added height and with constraints

--- lib/screens/add-player-screen.dart
+++ lib/screens/add-player-screen.dart
@@ -299,7 +299,7 @@ class _AddPlayerScreenState extends State<AddPlayerScreen> {
                     onPressed: () async {
                       final ImagePicker _picker = ImagePicker();
                       pickedImage = await _picker.pickImage(
-                          source: ImageSource.camera, imageQuality: 10);
+                          source: ImageSource.camera, maxHeight: 640, maxWidth: 480);
                       if (pickedImage == null) {
                         return;
                       }
