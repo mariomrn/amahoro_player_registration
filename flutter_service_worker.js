e6eeda0c35ee4f00ad692a5c34b70a030054344a
mariomrn <mariomrn@users.noreply.github.com>
2023-05-14 18:55:50 +0000

    deploy: e613f6a8cb619fee9b25e7369be88ce24298fe5b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,15 +14,15 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "7aeda5f8b090c9339b97ea3ed0d30dbf",
-"index.html": "d4a68b384a29992406f6ac58a4e4e4cb",
-"/": "d4a68b384a29992406f6ac58a4e4e4cb",
+"index.html": "5d9509155e9796b1ae36130c35e3aea4",
+"/": "5d9509155e9796b1ae36130c35e3aea4",
 "favicon.png": "52df10a304fd2fd8c6f0d287e47133e8",
 "version.json": "0099ef9015c2598eb3219c70959b156e",
 "icons/Icon-maskable-192.png": "41b3a3738f7a3e6eddd24343c4bd8000",
 "icons/Icon-192.png": "41b3a3738f7a3e6eddd24343c4bd8000",
 "icons/Icon-512.png": "ad6e24ee89aadd52d9873c2d9232902a",
 "icons/Icon-maskable-512.png": "ad6e24ee89aadd52d9873c2d9232902a",
-"main.dart.js": "6d0b8ac1496c3d7a181e2057389e306b",
+"main.dart.js": "963110bba81371cb8963a9b08539a902",
 "manifest.json": "5fefc70adcce3f4d7ff78169c1202c78"
 };
 
